b81068b6fbffa22b6bc3f23844dc5daf8301d265
Amutha <67087939+Amutha37@users.noreply.github.com>
2021-02-11 09:51:04 +1100

    Add files via upload

---
+++ appendremove/main.js
@@ -0,0 +1,15 @@
+// add a new item into the list
+let newDestination = document.createElement("li");
+
+newDestination.id = "oaxaca";
+
+newDestination.innerHTML = "Kishen, Mexico";
+
+document.getElementById("more-destinations").appendChild(newDestination);
+
+// removing element
+// let oax = document.querySelector("#oaxaca");
+// const child = document.getElementById("oaxaca");
+// const parent = document.querySelector("#more-destinations");
+
+// parent.removeChild(child);

4b4975ce4c3e710dd8f6ae8a2e545ea9f93606f8
Amutha <67087939+Amutha37@users.noreply.github.com>
2021-05-11 08:17:14 +1000

    Add files via upload
    Textbubble and jsbubble have the same codes. They are from Codecademy  initial lesson samples. I could not find the way to rename. Textbubble is easier to understand the codes purpose.

---
+++ textbubble/main.js
@@ -0,0 +1,16 @@
+var wordToDisplay1 = "Super mothers !";
+// var wordToDisplay2 = "You are always loved!";
+
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors1 = [red, orange, green, blue, purple];
+// var letterColors2 = [purple, orange, green, red, orange];
+
+drawName(wordToDisplay1, letterColors1);
+// drawName(wordToDisplay2, letterColors2);
+bubbleShape = "square";
+
+bounceBubbles();
